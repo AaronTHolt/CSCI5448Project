5d546f57d8556e5c2fd292d1876f6909b81668e7
Kyle Olsen <kyol3479@colorado.edu>
2016-04-04 16:41:00 -0600

    Added basic menu functionality

--- src/menu/welcomestate.cpp
+++ src/menu/welcomestate.cpp
@@ -1,21 +1,79 @@
 #include "welcomestate.h"
+#include<iostream>
 
 WelcomeState::WelcomeState(QGLWidget* context) : GameState(context)
 {
-    cube = new Cube(0.0, 0.0, 0.0,
-                    0.1, 0.1, 0.1);
+    title = new Cube(0.0, 2.1, 0.0,
+                     2.5, 0.6, 0.1);
+
+    playGame = new Cube(0.0, 0.9, 0.0,
+                        1.2, 0.3, 0.1);
+    selectableOptions.append(playGame);
+
+    changeProfile = new Cube(0.0, 0.1, 0.0,
+                             1.2, 0.3, 0.1);
+    selectableOptions.append(changeProfile);
+
+    settings = new Cube(0.0, -0.7, 0.0,
+                        1.2, 0.3, 0.1);
+    selectableOptions.append(settings);
+
+    stats = new Cube(0.0, -1.5, 0.0,
+                     1.2, 0.3, 0.1);
+    selectableOptions.append(stats);
+
+    exitGame = new Cube(0.0, -2.3, 0.0,
+                        1.2, 0.3, 0.1);
+    selectableOptions.append(exitGame);
+
+    selectedOption = 0;
+
+    highlightOption(selectedOption);
 }
 
 void WelcomeState::draw()
 {
-    cube->draw();
+    title->draw();
+
+    for(int i = 0; i < selectableOptions.size(); ++i)
+    {
+        selectableOptions.at(i)->draw();
+    }
+
 }
 
 void WelcomeState::keyPressEvent(QKeyEvent* event)
 {
-    if (event->key() == Qt::Key_Space)
+    if (event->key() == Qt::Key_Up)
+    {
+        restoreOption(selectedOption);
+        --selectedOption;
+        if (selectedOption <= -1)
+        {
+            selectedOption = selectableOptions.size() - 1;
+        }
+        highlightOption(selectedOption);
+    }
+    else if (event->key() == Qt::Key_Down)
+    {
+        restoreOption(selectedOption);
+        ++selectedOption;
+        selectedOption = selectedOption % selectableOptions.size();
+        highlightOption(selectedOption);
+    }
+    else if (event->key() == Qt::Key_Space)
     {
         GameStateContext* context = dynamic_cast<GameStateContext*>(gameStateContext);
-        context->setState(context->ProfileSelect);
+        context->setState(context->Playable);
     }
+}
+
+void WelcomeState::highlightOption(int option)
+{
+    selectableOptions.at(option)->rotate(180.0);
+}
+
+void WelcomeState::restoreOption(int option)
+{
+    selectableOptions.at(option)->rotate(0.0);
 }
--- src/menu/welcomestate.h
+++ src/menu/welcomestate.h
@@ -10,6 +10,21 @@ public:
     WelcomeState(QGLWidget*);
     void draw();
     void keyPressEvent(QKeyEvent*);
+
+private:
+    void highlightOption(int option);
+    void restoreOption(int option);
+
+    Cube* title;
+
+    Cube* playGame;
+    Cube* changeProfile;
+    Cube* settings;
+    Cube* stats;
+    Cube* exitGame;
+    QVector<Cube*> selectableOptions;
+
+    int selectedOption;
 };
 
 #endif // _WELCOMESTATE_H
