7fe98b4f288bc45510ee831f5f3264f74af2e0b2
David Baird <david.baird@colorado.edu>
2016-04-05 12:58:02 -0600

    Adding a 3D vector class as a utility

---
+++ src/util/vector3.cpp
@@ -0,0 +1,39 @@
+#include "vector3.h"
+
+Vector3::Vector3(){
+  vec = btVector3(0.0, 0.0, 0.0);
+}
+
+Vector3::Vector3(float x, float y, float z){
+  vec = btVector3(x,y,z);
+}
+
+Vector3::Vector3(const Vector3& other){
+  vec = other.vec;
+}
+
+Vector3::Vector3(const btVector3& other){
+  vec = other;
+}
+
+Vector3& Vector3::operator=(const Vector3& other){
+  vec = other.vec;
+  return *this;
+}
+
+Vector3& Vector3::operator=(const btVector3& other){
+  vec = other;
+  return *this;
+}
+
+float Vector3::getX() const{
+  return vec.x();
+}
+
+float Vector3::getY() const{
+  return vec.y();
+}
+
+float Vector3::getZ() const{
+  return vec.z();
+}
