8e1814c38089d8fe0968f6167f18f73376cc27b7
David Baird <david.baird@colorado.edu>
2016-04-13 17:41:34 -0600

    Giving Projectiles their GameObjectType

--- src/playableObjects/gameObjects/projectile.h
+++ src/playableObjects/gameObjects/projectile.h
@@ -10,15 +10,15 @@
 class Projectile: public GameObject{
 	private:
         bool isInPlay;
-        int type;
+        int projectileType;
         int bulletNumber; //keep track of which bullet to delete
         Cube* proj;
         Weapon* w;
         
     public:
-		Projectile(int t, Vector3 p, Vector3 f, Weapon* w = nullptr);
-        ~Projectile();
-		bool onCollision(GameObjectType got);
+	Projectile(int t, Vector3 p, Vector3 f, Weapon* w = nullptr);
+	~Projectile();
+	bool onCollision(GameObjectType got);
         void draw() const;
         void setIsInPlay(bool exists);
         bool getIsInPlay();
